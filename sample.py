a73b70179d537b872d1a43ae91653b961ec47c0c
umashankar43631 <shankar.mulakalapalli@gmail.com>
2025-07-31 20:25:59 +0530

    added folder and did some changes in the sample.py

--- sample.py
+++ sample.py
@@ -6,3 +6,6 @@ print(" I am a Data scientist")
 
 print(" I have invited nitesh as collaborator")
 print("I have added nitesh as a collaborator")
+
+a = 10
+print(" I am printing b, oh no, I forgot to define b i defined a instead")
